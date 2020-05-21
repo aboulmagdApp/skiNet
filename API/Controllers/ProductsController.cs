5e9d9268feb0fe9511f8a1201ea7189e5a745e12
Mohamed Aboulmagd <aboulmagd@live.com>
2020-05-22 01:47:17 +0300

    Adding thExtending the products entity and creating related entitiese repository methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,23 +12,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _Repo;
+        public ProductsController(IProductRepository Repo)
         {
-            _context = context;
+            _Repo = Repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-           var products = await _context.Products.ToListAsync();
-           return Ok(products);
+            var products = await _Repo.GetProductsAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _Repo.GetProductByIdAsync(id);
         }
     }
 }

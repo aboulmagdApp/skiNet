6c42d2bc805f6c0ff61582b396f0234c5ff77844
Mohamed Aboulmagd <aboulmagd@live.com>
2020-05-24 22:45:25 +0300

    Implementing the methods in the Generic repository

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -2,19 +2,21 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Data
-{
-    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
-    {
-        public Task<T> GetByIdAsync(int id)
-        {
-            throw new System.NotImplementedException();
+namespace Infrastructure.Data {
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity {
+        private readonly StoreContext _context;
+        public GenericRepository (StoreContext context) {
+            _context = context;
         }
 
-        public Task<IReadOnlyList<T>> ListAllAsync()
-        {
-            throw new System.NotImplementedException();
+        public async Task<T> GetByIdAsync (int id) {
+           return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync () {
+            return await _context.Set<T>().ToListAsync();
         }
     }
 }
